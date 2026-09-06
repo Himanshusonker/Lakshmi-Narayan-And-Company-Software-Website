b20c295615c82d286e426c9fe817be58eb8d2b01
Himanshusonker <himanshusonker90@gmail.com>
2026-09-06 12:47:18 +0530

    Update About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -128,7 +128,6 @@ const About=()=>{
 
                         {aboutData.hero?.image && (
 
-                            // <img src={aboutData.hero.image} alt="Lakshmi Narayan And Company" loading="eager" decoding="async" />
                             <img src={aboutData.hero.image} alt="Lakshmi Narayan And Company" />
                         )}
 
